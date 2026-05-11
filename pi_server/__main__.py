d3e0d62ec0bf3098bf3d868f370234bf37e0b56e
Jorgeap64 <jorge.duarte.a.p.21@gmail.com>
2026-05-11 18:15:47 +0100

    chore(utils): initial setup for logging and build structure
    - Create pi_server/utils/logger.py for centralized logging logic.
    - Update .gitignore to exclude the logs directory.

--- pi_server/__main__.py
+++ pi_server/__main__.py
@@ -1,5 +1,18 @@
+from pi_server.utils.logger import get_logger
+
+
+"""
+===============================================================================
+
+	Main
+
+===============================================================================
+"""
+
+log = get_logger("server.log", console=True)
+
 def main():
-    print("Hello World!")
+    log.info("Hello World!")
 
 if __name__ == "__main__":
     main()
